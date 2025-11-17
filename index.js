c9cf3b932606c4ae423a928ce264bb280436d6e4
Sydrek <routx40@gmail.com>
2025-11-17 09:35:15 +0200

    added comments

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+
+//CommonJS on vana ning internetis on veel näiteid selle kohta
 const common = require('./common.js')
 
+//import common from './common.js'
 common.hello()
 
+//Parem on kasutada importi
 import es from './es.js'
 es.hello()
+
