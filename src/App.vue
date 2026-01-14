89dfd628e9056add0c12544ee4db6d98ed620b3a
Sydrek <133598657+Sydrek@users.noreply.github.com>
2026-01-14 14:06:07 +0200

    added Vue & Vite config

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+    let message = 'Hello Vue!'
+</script>
+
+<template>
+    <h1>hello Vue</h1>
+    <H2>{{ message }}</H2>
+</template>
+
+<style>
+
+</style>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello Vite!')
+import { createApp } from 'vue';
+// import the root component App from a single-file component.
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app');
