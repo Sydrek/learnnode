05f6e210137cb204a2e9b2af6987dd0069b34cc9
Sydrek <133598657+Sydrek@users.noreply.github.com>
2026-01-28 13:12:12 +0200

    added setup notes + app.vue code
    the code is not usable. This commit is just to save the code as a reference

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup>
-    let message = 'Hello Vue!'
+
+import { ref } from 'vue';
+
+let message = ref('Hello Vue!');
+
+// function input(event){
+//     console.log(event.target.value);
+//     message.value = event.target.value;
+// }
+
 </script>
 
 <template>
-    <h1>hello Vue</h1>
-    <H2>{{ message }}</H2>
+    <div class="container">
+        <h1>{{ message.split('').reverse().join('') }}</h1>
+        <button class="button is-primary" @click="message = 'Hello TA-24A!'">click me!</button>
+        <input class="input" v-model="message">
+    </div>
 </template>
 
 <style>
