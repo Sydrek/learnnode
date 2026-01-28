a0748d9770a8a2f52f5d15f732719ba8fe8829a7
Sydrek <133598657+Sydrek@users.noreply.github.com>
2026-01-28 13:49:15 +0200

    21.01.25 Tund - ToDoList
    Turned App.vue into a ToDoList with checkboxes and adding function

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,64 @@
 <script setup>
 
-import { ref } from 'vue';
+// Imports
+import { computed, ref } from 'vue';
 
-let message = ref('Hello Vue!');
+// Simple Variables
+let i = 0;
+let items = ref([
+    {id: i++, text: 'Sai', isDone: false},
+    {id: i++, text: 'Piim', isDone: false},
+    {id: i++, text: 'Viin', isDone: false}
+]);
+let newItem = ref('');
 
-// function input(event){
-//     console.log(event.target.value);
-//     message.value = event.target.value;
-// }
+// Complex Variables
+let doneItems = computed(() => items.value.filter(i => i.isDone));
 
+// Functions
+function add() {
+    if (newItem.value.trim() !== '') {
+        items.value.push({id: i++, text: newItem.value.trim(), isDone: false});
+    }
+    newItem.value = '';
+}
 </script>
 
 <template>
     <div class="container">
-        <h1>{{ message.split('').reverse().join('') }}</h1>
-        <button class="button is-primary" @click="message = 'Hello TA-24A!'">click me!</button>
-        <input class="input" v-model="message">
+
+        <div class="field has-addons mt-5">
+
+            <div class="control is-expanded">
+                <input class="input" type="text" v-model="newItem" @keypress.enter="add">
+            </div>
+
+            <div class="control">
+                <button class="button is-info" @click="add">
+                    Add item
+                </button>
+            </div>
+
+        </div>
+
+        <div class="content">
+            <h1>All Items</h1>
+            <ul>
+                <li v-for="item in items">
+                    {{ item.text }}
+                    <input type="checkbox" v-model="item.isDone">
+                </li>
+            </ul>
+
+            <h1>Done Items</h1>
+            <ul>
+                <li v-for="item in doneItems">
+                    {{ item.text }}
+                    <input type="checkbox" v-model="item.isDone">
+                </li>
+            </ul>
+        </div>
+
     </div>
 </template>
 
